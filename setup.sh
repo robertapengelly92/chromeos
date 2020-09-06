b97e6f4f9e65637522c0f74324b8b9ec0d02835d
Robert Pengelly <robertapengelly@hotmail.co.uk>
2020-09-06 17:17:23 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -15,9 +15,6 @@ apt -y install qemu qemu-block-extra qemu-kvm qemu-slof qemu-system qemu-user qe
 apt -y install bochs bochsbios bochs-x vgabios || exit 1
 apt -y install bless deluge gdebi-core ghex gimp isomaster python3 python3-pip || exit 1
 
-chmod 4711 /usr/bin/wodim
-chmod 4711 /usr/bin/cdrdao
-
 apt -y install apt-transport-https curl
 curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
 echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | tee /etc/apt/sources.list.d/brave-browser-release.list
